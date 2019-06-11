84ff8dd1ffa1c6bb4b7a1cb2d412456aab9c912b
Gabe Bravo <gabecbravo@gmail.com>
2019-06-11 15:16:51 -0400

    Added startup files

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const age = 39;
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Ionicons name="ios-pizza" color="red" size={100} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#ff0000'
+  }
+});
+
+export default App 
