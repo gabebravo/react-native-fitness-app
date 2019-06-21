211f6dca1425c77739655a1765a7d0bdf189eb53
Gabe Bravo <gabecbravo@gmail.com>
2019-06-21 15:40:15 -0400

    Adedd some styling and a submit button

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import AddEntry from './components/AddEntry'
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100
+  }
+});
 
 class App extends React.Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <AddEntry />
       </View>
     );
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,57 +1,82 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import Steppers from './Steppers'
 import Slider from './Slider'
 import DateHeader from './DateHeader'
 
+const DEFAULT_STATE = {
+  run: 0,
+  bike: 0,
+  swim: 0,
+  sleep: 0,
+  eat: 0
+}
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
+
 class AddEntry extends Component {
-  state = {
-    run: 0,
-    bike: 0,
-    swim: 0,
-    sleep: 0,
-    eat: 0
-  }
+  state = DEFAULT_STATE
 
   increment = (metric) => {
     const { max, step } = getMetricMetaInfo(metric)
-    this.setState( (state) => {
+    this.setState((state) => {
       const count = state[metric] + step
-      return {...state, [metric]: count > max ? max : count }
-    }) 
+      return { ...state, [metric]: count > max ? max : count }
+    })
   }
 
   decrement = (metric) => {
-    this.setState( (state) => {
+    this.setState((state) => {
       const count = state[metric] - getMetricMetaInfo(metric).step
-      return {...state, [metric]: count < 0 ? 0 : count }
-    }) 
+      return { ...state, [metric]: count < 0 ? 0 : count }
+    })
   }
 
   slide = (metric, value) => {
-    this.setState({ [metric]: value }) 
+    this.setState({ [metric]: value })
+  }
+
+  submit = () => {
+    const key = timeToString();
+    const entry = { ...this.state }
+
+    this.setState(DEFAULT_STATE)
+
+    // TODO : 
+    // 1) UPDATE REDUX
+    // 2) NAVIGATE HOME
+    // 3) SAVE TO DB
+    // 4) CLEAR LOCAL NOTIFICATIONS
   }
 
   render() {
     const metaInfo = getMetricMetaInfo()
     return (
       <View>
-        <DateHeader date={ (new Date()).toLocaleDateString() } />
-        {Object.keys(metaInfo).map( key => {
-          const { getIcon, type, ...rest} = metaInfo[key]
+        <DateHeader date={(new Date()).toLocaleDateString()} />
+        <Text>{JSON.stringify(this.state)}</Text>
+        {Object.keys(metaInfo).map(key => {
+          const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
 
           return (
             <View key={key}>
               {getIcon()}
-              { type === 'slider' ? 
+              {type === 'slider' ?
                 <Slider value={value} onChange={() => this.slide(key, value)} {...rest} /> :
                 <Steppers value={value} onIncrement={() => this.increment(key)} onDecrement={() => this.decrement(key)} {...rest} />
               }
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
