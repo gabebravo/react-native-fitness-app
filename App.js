38202bc36cb04a735320ff62fe8017ab147503e8
Gabe Bravo <gabecbravo@gmail.com>
2019-06-21 15:59:41 -0400

    Added the slider

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import AddEntry from './components/AddEntry'
+import { View, Text, StyleSheet, Slider } from 'react-native';
+// import AddEntry from './components/AddEntry'
 
 const styles = StyleSheet.create({
   container: {
@@ -14,10 +14,21 @@ const styles = StyleSheet.create({
 
 class App extends React.Component {
 
+  state = {
+    value: 0
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <AddEntry />
+        <Slider 
+          value={this.state.value} 
+          step={1}
+          minimumValue={-10} 
+          maximumValue={10} 
+          onValueChange={value => this.setState({ value })} 
+        />
+        <Text>Value: {this.state.value}</Text>
       </View>
     );
   }
